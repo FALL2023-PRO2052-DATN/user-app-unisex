90095169ef73c32f807f4bbfeef17f65031eaa99
ADMIN <xuanboy0911>
2023-10-06 21:22:09 +0700

    register/tu

--- app/src/main/java/com/example/shopclothes/view/activity/register/RegisterActivity.java
+++ app/src/main/java/com/example/shopclothes/view/activity/register/RegisterActivity.java
@@ -5,12 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.shopclothes.R;
+import com.example.shopclothes.databinding.ActivityRegisterBinding;
+import com.example.shopclothes.utils.UIView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements RegisterContract.View {
+    ActivityRegisterBinding mActivityRegisterBinding;
+    RegisterContract.Presenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        mActivityRegisterBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(mActivityRegisterBinding.getRoot());
+        mPresenter = new RegisterPresenter(this);
+        onClick();
+    }
+
+    public void onClick() {
+        mActivityRegisterBinding.btnRegister.setOnClickListener(view -> register());
+    }
+
+    public void register() {
+        String email = mActivityRegisterBinding.etEmailRegister.getText().toString();
+        String password = mActivityRegisterBinding.etPasswordRegister.getText().toString();
+        String confirmPassword = mActivityRegisterBinding.etPasswordAgainRegister.getText().toString();
+        mPresenter.doRegister(email, password, confirmPassword);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        UIView.showMessage(mActivityRegisterBinding.getRoot(), message);
     }
 }
--- app/src/main/java/com/example/shopclothes/view/activity/register/RegisterPresenter.java
+++ app/src/main/java/com/example/shopclothes/view/activity/register/RegisterPresenter.java
@@ -1,4 +1,35 @@
 package com.example.shopclothes.view.activity.register;
 
-public class RegisterPresenter {
+import com.example.shopclothes.constant.Constant;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class RegisterPresenter implements RegisterContract.Presenter {
+    private final RegisterContract.View view;
+    private final FirebaseAuth mAuth;
+
+    public RegisterPresenter(RegisterContract.View view) {
+        this.view = view;
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void doRegister(String email, String password, String confirmPassword) {
+        if (!password.equals(confirmPassword)) {
+            view.onMessage("Passwords Incorrect");
+            return;
+        }
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(executor, task -> {
+                    if (task.isSuccessful()) {
+                        view.onMessage(Constant.onSuccess);
+                    } else {
+                        view.onMessage(Constant.onError);
+                    }
+                });
+    }
 }
