1ee3d1a5e31d98d3bec12042c5edc12812d8b13f
ADMIN <xuanboy0911>
2023-10-26 09:20:59 +0700

    update bill

---
+++ app/src/main/java/com/example/shopclothes/view/activity/oderDetail/OderDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.shopclothes.view.activity.oderDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopclothes.R;
+
+public class OderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oder_detail);
+    }
+}
