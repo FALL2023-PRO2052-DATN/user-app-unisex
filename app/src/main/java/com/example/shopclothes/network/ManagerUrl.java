b5532bf5d1761103d0e1101f015829a35c9e8d24
Tys7603 <long.vlgt@gmail.com>
2023-11-11 22:24:25 +0700

    Complete Comment + Settings

--- app/src/main/java/com/example/shopclothes/network/ManagerUrl.java
+++ app/src/main/java/com/example/shopclothes/network/ManagerUrl.java
@@ -2,7 +2,7 @@ package com.example.shopclothes.network;
 
 public class ManagerUrl {
 
-    public static final String BASE_URL = "http://172.16.49.149:3000/api/";
+    public static final String BASE_URL = "http://192.168.1.14:3000/api/";
 
     // user
     public static final String INSERT_USER = "insert-user";
